24169cf048625fd81a2de15b95ac0a0e8172c79d
fabianberisha <50098640+fabianberisha@users.noreply.github.com>
2022-09-08 18:12:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          Welcome to CI/CD session 
+            </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
